491e04ac7ff133bd59782c193656c8223f1c0329
Hackathon <hackathon@e1z4r6p19.42.us.org>
2017-12-13 12:36:42 -0800

    fixed module

---
+++ searchClover.js
@@ -0,0 +1,35 @@
+var request = require('request');
+
+cloverMerchants = [
+  {
+    id: '8T8VYFD236V4W',
+    token: '37231e0b-b510-6a35-076d-7376cc13e791'
+  }
+];
+
+module.exports.searchClover = (query) => {
+  let searchResults = [];
+  for (merchant in cloverMerchants) {
+    let result = {};
+    request.get(
+      'https://apisandbox.dev.clover.com/v3/merchants/' + merchant.id + '?=access_token=' + merchant.token,
+      (err, res, body) => {
+        result.merchant = JSON.parse(body);
+      }
+    );
+    request.get(
+      'https://apisandbox.dev.clover.com/v3/merchants/' + merchant.id + '/items?=access_token=' + merchant.token,
+      (err, res, body) => {
+        result.items = JSON.parse(body);
+      }
+    );
+    request.get(
+      'https://apisandbox.dev.clover.com/v3/merchants/' + merchant.id + '/orders?=access_token=' + merchant.token,
+      (err, res, body) => {
+        result.orders = JSON.parse(body);
+      }
+    );
+    searchResults.append(result);
+  }
+  return (searchResult);
+}
